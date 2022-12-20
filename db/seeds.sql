a4c22e6f8eb1fcf76fb1b41da4ef242cf6f61076
ana <ana@anniecannons.com>
2022-12-20 10:00:21 -0800

    added the functions  and the roles to update add and view all rolls

--- db/seeds.sql
+++ db/seeds.sql
@@ -16,11 +16,11 @@ INSERT INTO role
 (title, salary, department_id)
 
 VALUES
-('Sales Director', 200000, 1),
+('Sales Director', 100000, 1),
 ('HR Director', 100000, 2),
 ('Marketing Manager', 70000, 3),
-('Jr. Developer', 85000, 3),
-('Sr. Developer', 125000, 3),
+('Jr. Developer', 75000, 3),
+('Sr. Developer', 13000, 3),
 ('Development Manager', 100000, 4),
 ('IT Project Manager', 850000, 4),
 ('IT Project Director', 100000, 4);
@@ -32,11 +32,11 @@ INSERT INTO employee
 (first_name, last_name, role_id, manager_id)
 
 VALUES
-('Mike', 'Miller', 1, 2),
-('John', 'Winters', 2, 1),
-('Lisa', 'Wright', 3, 4),
-('Steve', 'Streets', 4, 3),
-('Chris', 'Maple', 5, 6),
-('Heather', 'Williams', 6, 5),
-('Julie', 'Adams', 7, 8),
-('Dan', 'Klein', 8, 7);
+('Conrad', 'Keynes', 1, 2),
+('Nikita', 'Lee', 2, 1),
+('Gia', 'Venter', 3, 4),
+('Marcelo', 'Barca', 4, 3),
+('Ben', 'Bingham', 5, 6),
+('Shari', 'Espinoza', 6, 5),
+('Wayland', 'Charles', 7, 8),
+('Rose', 'Calcar', 8, 7);
--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
-const cTable = require('console.table');
-
+ const cTable = require('console.table');
+ require("dotenv").config();
+//  const { prompt } = require('inquirer');
 // this connects mysql to the data base 
 const connection = mysql.createConnection({
     host: 'localhost',
-    port: 3000,
-    user: 'root',
-    password: 'rootroot',
-    database: 'employeeTracker_db',
-});
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+},
+console.log("We have connected to the database")
+);
 
-
+//this starts the program
 const menuPrompts = () => {
     inquirer
         .prompt([
@@ -30,7 +32,8 @@ const menuPrompts = () => {
                   'exit menu'
             ],
         }
-    ]).then((res)=>{
+        //this is the switch statement that will take the user to the correct function
+    ]).then((res)=> {
         switch (res.choiceList) {
             case 'View all departments':
                 viewAllDepartments();    
@@ -55,37 +58,193 @@ const menuPrompts = () => {
                 updateEmployeeRole();
             break;
             case 'Exit program':
-                connection.end();
-                console.log('You have exited the employee management program!');
-                return;
-            default:
-                break;
+                exitProgram();
+            break;
+
         }
     })
 }
 
-const viewAllEmployees = () => {
-   
+
+//this is the function that will start the program
+const viewAllDepartments = () => {
     connection.query(
-        'SELECT * FROM employee', (err, res) => {
+        `SELECT department.name AS department, employees.first_name, employees.last_name FROM employees LEFT JOIN role ON employees.role_id = role.id LEFT JOIN department ON role.department_id = department.id ORDER BY department.id`,
+        (err, res) => {
+            if (err) throw err;
+            cTable(res);
+            menuPrompts();
+        }
+    );
+};
+
+
+//  this is the function that will view all roles
+const viewAllRoles = () => {
+    connection.query(
+        `SELECT role.title, role.salary, department.name AS department FROM role LEFT JOIN department ON role.department_id = department.id`,
+        (err, res) => {
+            if (err) throw err;
+            cTable(res);
+            menuPrompts();
+        }
+    );
+};
+
+//  this is the function that will view all employees
+const viewAllEmployees = () => {
+    connection.query(
+      `SELECT employees.id, employees.first_name, employees.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employees LEFT JOIN role ON employees.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employees manager ON manager.id = employees.manager_id`,
+      (err, res) => {
         if (err) throw err;
         console.table(res);
-    })
-    menuPrompts();
-}
+        menuPrompts();
+      }
+    );
+  };
+
+//
+const addADepartment = () => {
+    inquirer
+        .prompt([
+            {
+                name: 'department',
+                type: 'input',
+                message: 'What is the name of the department you would like to add?',
+            },
+        ])
+        .then((res) => {
+            connection.query(
+                `INSERT INTO department SET ?`,
+                {
+                    name: res.department,
+                },
+                (err) => {
+                    if (err) throw err;
+                    console.log('The department was added successfully!');
+                    menuPrompts();
+                }
+            );
+        });
+};
+
+//  this is the function that will add a role to the database
+const addARole = () => {
+    inquirer
+        .prompt([
+            {
+                name: 'title',
+                type: 'input',
+                message: 'What is the title of the role you would like to add?',
+            },
+            {
+                name: 'salary',
+                type: 'input',
+                message: 'What is the salary of the role you would like to add?',
+            },
+            {
+                name: 'department',
+                type: 'input',
+                message: 'What is the department id of the role you would like to add?',
+            },
+        ])
+        .then((res) => {
+            connection.query(
+                `INSERT INTO role SET ?`,
+                {
+                    title: res.title,
+                    salary: res.salary,
+                    department_id: res.department,
+                },
+                (err) => {
+                    if (err) throw err;
+                    console.log('The role was added successfully!');
+                    menuPrompts();
+                }
+            );
+        });
+};
+
+//  this is the function that will add an employee to the database
+ const addAnEmployee = () => {
+    inquirer
+        .prompt([
+            {
+                name: 'firstName',
+                type: 'input',
+                message: 'What is the first name of the employee you would like to add?',
+            },
+            {
+                name: 'lastName',
+                type: 'input',
+                message: 'What is the last name of the employee you would like to add?',
+            },
+            {
+                name: 'role',
+                type: 'input',
+                message: 'What is the role id of the employee you would like to add?',
+            },
+            {
+                name: 'manager',
+                type: 'input',
+                message: 'What is the manager id of the employee you would like to add?',
+            },
+        ])
+        .then((res) => {
+            connection.query(
+                `INSERT INTO employees SET ?`,
+                {
+                    first_name: res.firstName,
+                    last_name: res.lastName,
+                    role_id: res.role,
+                    manager_id: res.manager,
+                },
+                (err) => {
+                    if (err) throw err;
+                    console.log('The employee was added successfully!');
+                    menuPrompts();
+                }
+            );
+        });
+};
+
+//  this is the function that will update an employee's role
+const updateEmployeeRole = () => {
+    inquirer
+        .prompt([
+            {
+                name: 'employee',
+                type: 'input',
+                message: 'What is the id of the employee you would like to update?',
+            },
+            {
+                name: 'role',
+                type: 'input',
+                message: 'What is the role id of the employee you would like to update?',
+            },
+        ])
+        .then((res) => {
+            connection.query(
+                `UPDATE employees SET role_id = ? WHERE id = ?`,
+                [res.role, res.employee],
+                (err) => {
+                    if (err) throw err;
+                    console.log('The employee was updated successfully!');
+                    menuPrompts();
+                }
+            );
+        });
+};
 
 
-
-// connection.connect((err) => {
-//     if (err) throw err;
-
-
-    
-
-// });
-      
+//  this is the function that will exit the program
+const exitProgram = () => {
+    connection.end();
+    console.log('You have exited the employee management program!');
+    return;
+};
 
 
+//this is the function that will start the program
+menuPrompts();
 
-
-    
